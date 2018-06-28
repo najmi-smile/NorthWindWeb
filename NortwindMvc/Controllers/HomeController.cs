8a8e2426c4559a442fdb8f5578fbd20b7bda72bc
najmi-smile <tahiranjamraza@gmail.com>
2018-06-28 21:52:02 +0100

    add NorthwindMvc

---
+++ NortwindMvc/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindContextLib;
+using NortwindMvc.Models;
+
+namespace NortwindMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Northwind db;
+        public HomeController(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
+
+      
+        public IActionResult Index()
+        {
+            var model = new HomeIndexViewModel
+            {
+                vistorCount = (new Random()).Next(1, 1001),
+                Categories = db.Categories.ToList(),
+                Products = db.Products.ToList()
+            };
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NortwindMvc/Models/HomeIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using NorthwindEntitiesLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NortwindMvc.Models
+{
+    public class HomeIndexViewModel
+    {
+        public int vistorCount;
+        public IList<Category> Categories { get; set; }
+        public IList<Product> Products { get; set; }
+
+    }
+}
